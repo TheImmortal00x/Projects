ec238ace0b187b7eae4a9ea8694a9cb9768372a3
TheImmortal00x <eternal.dragon1288@gmail.com>
2020-09-19 15:27:45 +0300

    initial commit

--- Need for JS/main.js
+++ Need for JS/main.js
@@ -2,13 +2,8 @@ const score = document.querySelector('.score'),
     start = document.querySelector('.start'),
     gameArea = document.querySelector('.gameArea'),
     car = document.createElement('div');
-car.classList.add('car');
 
-const setting = {
-    start: false,
-    score: 0,
-    speed: 3
-};
+car.classList.add('car');
 
 start.addEventListener('click', startGame);
 document.addEventListener('keydown', startRun);
@@ -17,20 +12,69 @@ document.addEventListener('keyup', stopRun);
 const keys = {
     ArrowUp: false,
     ArrowDown: false,
-    ArrowLeft: false,
-    ArrowRight: false
+    ArrowRight: false,
+    ArrowLeft: false
 };
 
+const setting = {
+    start: false,
+    score: 0,
+    speed: 3,
+    traffic: 3
+};
+
+function getQuantityElements(heightElement) {
+    return document.documentElement.clientHeight / heightElement + 1;
+}
+
 function startGame() {
+
+    for (let i = 0; i < 20; i++) {
+        const line = document.createElement('div');
+        line.classList.add('line');
+        line.style.top = (i * 100) + 'px';
+        line.y = i * 100;
+        gameArea.appendChild(line);
+    }
+
+    for (let i = 0; i < getQuantityElements(100 * setting.traffic); i++) {
+        const enemy = document.createElement('div');
+        enemy.classList.add('enemy')
+        enemy.y = 100 * setting, traffic * (i + 1);
+        enemy.style.top = enemy.y + 'px';
+        gameArea.appendChild(enemy);
+    }
+
     start.classList.add('hide');
     setting.start = true;
     gameArea.appendChild(car);
+    setting.x = car.offsetLeft;
+    setting.y = car.offsetTop;
     requestAnimationFrame(playGame);
 }
 
 function playGame() {
-    console.log("start game!")
+    moveRoad();
     if (setting.start) {
+        if (keys.ArrowLeft && setting.x > 0) {
+            setting.x -= setting.speed;
+        }
+        if (keys.ArrowRight && setting.x < (gameArea.offsetWidth - car.offsetWidth)) {
+            setting.x += setting.speed;
+        }
+
+        if (keys.ArrowDown && setting.y < (gameArea.offsetHeight - car.offsetHeight)) {
+            setting.y += setting.speed;
+        }
+
+        if (keys.ArrowUp && setting.y > 0) {
+            setting.y -= setting.speed;
+        }
+
+        car.style.left = setting.x + 'px';
+        car.style.top = setting.y + 'px';
+
+
         requestAnimationFrame(playGame)
     }
 }
@@ -40,7 +84,18 @@ function startRun(event) {
     keys[event.key] = true;
 }
 
-function stopTun(event) {
+function stopRun(event) {
     event.preventDefault();
     keys[event.key] = false;
+}
+
+function moveRoad() {
+    let lines = document.querySelectorAll('.line');
+    lines.forEach(function (line) {
+        line.y += setting.speed;
+        line.style.top = line.y + 'px';
+        if (line.y >= document.documentElement.clientHeight) {
+            line.y = -100;
+        }
+    })
 }
